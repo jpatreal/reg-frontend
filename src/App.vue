043bf7271dc99c249403f2b41192fc793926ddc3
jpatreal <58502972+jpatreal@users.noreply.github.com>
2020-02-02 10:02:56 +0800

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <Toolbar />
+    <v-content class="my-2" style="background-color: #F5F5F5;">
+      <v-container pa-0>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Toolbar from './views/Toolbar'
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: 'App',
+  components: {
+    Toolbar
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container pa-2>
+    <v-layout row wrap align-center>
+      <v-flex>
+        <span class="headline font-weight-medium grey--text">Events</span>
+      </v-flex>
+    </v-layout>
+    <v-layout ma-3>
+      <v-flex xs4 mx-2>
+        <v-card>
+          <v-img
+            height="200px"
+          >
+            <v-card-title>Press Conference</v-card-title>
+          </v-img>
+          <v-card-subtitle class="pb-0">Davao City Press Conference</v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>20 Feb 2020</div>
+
+            <div>Start Event: 8:30am</div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              text
+              color="success"
+            >
+            Register
+            </v-btn>
+            <v-btn
+              text
+              color="primary"
+            >
+            See More Details
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs4 mx-2>
+        <v-card>
+
+        </v-card>
+      </v-flex>
+      <v-flex xs4 mx-2>
+        <v-card>
+
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: 'Home',
+  data () {
+
   }
 }
 </script>
+
+<style scoped>
+
+</style>
